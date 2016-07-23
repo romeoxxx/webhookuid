2fdbea082e0c86de107b3f4351dcafd0f3150e06
romeoxxx <romeox@romeoxxx.local>
2016-07-23 09:21:52 +0700

    psajd

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-
-
 $verify_token = "atp"; // Verify token
-$token = "EAAB16RCqjswBAIER9W93fqdX3SsSz71VJTZBsSyVXyJAeZCrXGHCNwy8AaNmGBfPctOPXWov5p5hG0970rwUiu2oI8opaNrKLlPdXb9ZCtxFVws8Mzd1KWnwnMyeY2yTs2P8wGdryYeZBph4SUG6kMiKq96oSKFqjPjrh5afhgZDZD"; // Page token
-
 // Receive something
 if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
 
@@ -12,6 +8,8 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
 } else {
 
     $data = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS_STRING);
-    $output = print_r($data,1);
-    $myfile = file_put_contents('logs.txt', $output.PHP_EOL , FILE_APPEND);
+    if (!empty($data['entry'][0]['changes'][0]['value']['user_id'])) {
+    	$output = $data['entry'][0]['changes'][0]['value']['user_id'];
+    	$myfile = file_put_contents('logs.txt', $output.PHP_EOL , FILE_APPEND);
+    }
 }
