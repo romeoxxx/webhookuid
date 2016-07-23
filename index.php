523a67e4087908106eeff3d46c3a0d15ca0eb5be
romeoxxx <romeox@romeoxxx.local>
2016-07-23 09:41:19 +0700

    ddc
    c

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_R
     $data = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS_STRING);
     if (!empty($data['entry'][0]['changes'][0]['value']['user_id'])) {
     	$uid = $data['entry'][0]['changes'][0]['value']['user_id'];
-    	file_get_contents("https://tiepcankhachhang.com/like/add.php?uid=".$uid);
+    	$data = file_get_contents("http://tiepcankhachhang.com/like/add.php?uid=".$uid);
     }
 }
